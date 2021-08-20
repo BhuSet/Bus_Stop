29d7c5aadb190a2599da5b75cbf5168938d42669
Bhuvi Sethuraman <Bhuvi.Sethuraman@softwire.com>
2021-08-20 12:08:24 +0100

    Invalid postcode error handling

--- Bus_Stop.js
+++ Bus_Stop.js
@@ -1,5 +1,8 @@
 const readline = require("readline-sync");
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
+const NUM_BUSES = 5;
+const NUM_NEAREST_STOPPOINTS = 2;
+const RADIUS = 500;
 
 /*console.log("Enter the Stop Code :");
 const Stop_id = readline.prompt();
@@ -14,7 +17,7 @@ function getNextfivebuses(json)
     //console.log(json);
     json.sort((a, b) => {return (a.timeToStation > b.timeToStation) - (a.timeToStation < b.timeToStation)});
     
-    for(var i=0; i<5 && i < json.length;i++)
+    for(var i=0; i<NUM_BUSES && i < json.length;i++)
         console.log(json[i].lineId+ "\t\t" +json[i].stationName+ "\t\t" +json[i].destinationName+ "\t\t"+ Math.ceil(json[i].timeToStation/60));
 }
 
@@ -24,7 +27,7 @@ var stoppointids= [];
 function getNearestStopPointId(json)
 {
     
-    for(var i = 0; i<json.stopPoints.length && stoppointids.length<2 && json.stopPoints[i].lines.length !=0; i++)
+    for(var i = 0; i<json.stopPoints.length && stoppointids.length < NUM_NEAREST_STOPPOINTS && json.stopPoints[i].lines.length !=0; i++)
         stoppointids.push(json.stopPoints[i].id);
     console.log(stoppointids);
     console.log("Bus Number\tFrom\t\t\tDestination\tArrvial in minutes\n");
@@ -36,7 +39,7 @@ function getNearestStopPointId(json)
 
 function getAreaStopPoints(json)
 {
-    fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${json.result.latitude}&lon=${json.result.longitude}&stopTypes=NaptanPublicBusCoachTram`)
+    fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${json.result.latitude}&lon=${json.result.longitude}&stopTypes=NaptanPublicBusCoachTram&radius=${RADIUS}`)
     .then(response => response.json())
     .then(getNearestStopPointId);
 }
@@ -45,5 +48,13 @@ console.log("Enter the PostCode :");
 const Post_code = readline.prompt();
 
 fetch(`http://api.postcodes.io/postcodes/${Post_code}`)
-.then(response => response.json())
-.then(getAreaStopPoints);
+.then(response => {
+    if(response.status >= 200 && response.status <= 299)
+        return response.json();
+    else
+        throw Error(response.status);
+})
+.then(getAreaStopPoints)
+.catch((error) => {
+                    (error.message === "404") ? console.log("Invalid Postcode") : console.log("Network Error")
+                  });
