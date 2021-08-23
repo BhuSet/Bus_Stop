81e098a5c72924388dc6b34176ff68c84f70e2df
BhuSet <89073847+BhuSet@users.noreply.github.com>
2021-08-23 11:50:58 +0100

    Add files via upload

--- Bus_Stop.js
+++ Bus_Stop.js
@@ -1,47 +1,56 @@
 const readline = require("readline-sync");
 const fetch = require('node-fetch');
-const NUM_BUSES = 5;
+const FIRST_N_BUSES =5;
 const NUM_NEAREST_STOPPOINTS = 2;
 const RADIUS = 500;
 
-/*console.log("Enter the Stop Code :");
-const Stop_id = readline.prompt();
+const getEachStopPointArrivals =(pointid) =>{
+    fetch(`https://api.tfl.gov.uk/StopPoint/${pointid}/Arrivals`)
+            .then(response => response.json())
+            .then((json) => {
+            if (json.length === 0)  
+                throw `There are no Buses running in stopID ${pointid}`;
+            else
+                return json;
+            })
+            .then(getFirstNbuses)
+            .catch((error) => console.log(error)); 
+}
 
-fetch(`https://api.tfl.gov.uk/StopPoint/${Stop_id}/Arrivals`)
-    .then(response => response.json())
-    .then(getNextfivebuses);
-*/
-
-function getNextfivebuses(json)
-{
+const getFirstNbuses= (json) => {
     //console.log(json);
     json.sort((a, b) => {return (a.timeToStation > b.timeToStation) - (a.timeToStation < b.timeToStation)});
     
-    for(var i=0; i<NUM_BUSES && i < json.length;i++)
+    for(var i=0; i< FIRST_N_BUSES && i < json.length;i++)
         console.log(json[i].lineId+ "\t\t" +json[i].stationName+ "\t\t" +json[i].destinationName+ "\t\t"+ Math.ceil(json[i].timeToStation/60));
 }
 
 
 var stoppointids= [];
 
-function getNearestStopPointId(json)
-{
+const getNearestStopPointId = (json) =>{
     
-    for(var i = 0; i<json.stopPoints.length && stoppointids.length < NUM_NEAREST_STOPPOINTS && json.stopPoints[i].lines.length !=0; i++)
+    for(var i = 0; i<json.stopPoints.length && stoppointids.length< NUM_NEAREST_STOPPOINTS /*&& json.stopPoints[i].lines.length !=0*/; i++)
         stoppointids.push(json.stopPoints[i].id);
     console.log(stoppointids);
-    console.log("Bus Number\tFrom\t\t\tDestination\tArrvial in minutes\n");
-    stoppointids.forEach(stoppointid =>
-            fetch(`https://api.tfl.gov.uk/StopPoint/${stoppointid}/Arrivals`)
-            .then(response => response.json())
-            .then(getNextfivebuses)); 
 }
 
-function getAreaStopPoints(json)
-{
+const getBusesforEachStopPoint =(json) =>{
+    console.log("Bus Number\tFrom\t\t\tDestination\tArrvial in minutes\n");
+    stoppointids.forEach(getEachStopPointArrivals); 
+}
+
+const getAreaStopPoints = (json) => {
     fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${json.result.latitude}&lon=${json.result.longitude}&stopTypes=NaptanPublicBusCoachTram&radius=${RADIUS}`)
     .then(response => response.json())
-    .then(getNearestStopPointId);
+    .then((json) => {
+    if (json.stopPoints.length === 0)  
+        throw `There are no Bus Stops in ${RADIUS}m`;
+    else
+        return json;
+    })
+    .then(getNearestStopPointId)
+    .then(getBusesforEachStopPoint);
 }
 
 console.log("Enter the PostCode :");
@@ -49,12 +58,13 @@ const Post_code = readline.prompt();
 
 fetch(`http://api.postcodes.io/postcodes/${Post_code}`)
 .then(response => {
-    if(response.status >= 200 && response.status <= 299)
-        return response.json();
-    else
-        throw Error(response.status);
-})
+    if (response.status >= 200 && response.status <= 299) 
+      return response.json();
+    else 
+      throw Error(response.status);
+    
+  })
 .then(getAreaStopPoints)
-.catch((error) => {
-                    (error.message === "404") ? console.log("Invalid Postcode") : console.log("Network Error")
-                  });
+.catch((Error) =>  {
+                    (Error.message == "404") ? console.log("Invalid Postcode"):console.log("Network Error")
+                    });
